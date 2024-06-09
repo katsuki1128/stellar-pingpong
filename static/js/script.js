1aba43077dc3a0e5609eae0a0eb6f2adda9d51d8
katsuki1128 <katsuki1128@gmail.com>
2024-06-09 11:30:21 +0900

    flex

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
 // static/js/script.js
+let maxDistance = 1000 * 60 * 60 * 20; // 20時間をミリ秒に変換
+
+const updateRangeValue = (value) => {
+    document.getElementById('range-value').textContent = value;
+    maxDistance = 1000 * 60 * 60 * value; // スライダーの値をミリ秒に変換
+    updateCountdown(); // 値が変更されたら即座に反映する
+};
 
 const updateCountdown = () => {
     const events = JSON.parse(document.getElementById('events-data').textContent);
@@ -7,8 +14,9 @@ const updateCountdown = () => {
     // console.log(events);
 
     const now = new Date().getTime();
-    const imgElement = document.getElementById('satellite-img');
-    const maxDistance = 20 * 60 * 60 * 1000; // 20時間をミリ秒に変換
+
+    // const imgElement = document.getElementById('satellite-img');
+    // const maxDistance = 20 * 60 * 60 * 1000; // 20時間をミリ秒に変換
 
     events.forEach((event, index) => {
         const eventTime = new Date(event.time).getTime();
@@ -23,9 +31,9 @@ const updateCountdown = () => {
         countdownElement.innerHTML = `${days}日 ${hours}時間 ${minutes}分 ${seconds}秒`;
 
         // 移動範囲を設定 (例として、0から80%の範囲で移動)
-        const totalDistance = 1000 * 60 * 60 * 20; // 20時間をミリ秒に変換
+        // const totalDistance = 1000 * 60 * 60 * 20; // 20時間をミリ秒に変換
         const maxLeft = window.innerWidth * 0.8; // 80%の範囲で移動
-        const leftPosition = Math.max(0, Math.min(maxLeft, maxLeft * (1 - distance / totalDistance)));
+        const leftPosition = Math.max(0, Math.min(maxLeft, maxLeft * (1 - distance / maxDistance)));
 
         const satelliteImg = document.getElementById(`satellite-img-${index}`);
         satelliteImg.style.left = `${leftPosition}px`;
